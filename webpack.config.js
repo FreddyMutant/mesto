e441dc4c6653ab1dc99be77e36bca572b8cd6afc
FreddyMutant <fedorasan@mail.ru>
2023-05-25 01:47:38 +0300

    9пр final commit

--- webpack.config.js
+++ webpack.config.js
@@ -9,33 +9,35 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-    publicPath: '',
-  },
+      filename: 'main.js',
+        publicPath: ''
+      },
   mode: 'development',
   devServer: {
     static: path.resolve(__dirname, './dist'),
-    open: true,
     compress: true,
-    port: 8080
+    port: 8080,
+    open: true
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: '/node_modules/'
+      },
+      {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource',
+        type: 'asset/resource'
       },
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader'
-        ]
-      },
+          loader: 'css-loader',
+          options: { importLoaders: 1 }
+        },
+        'postcss-loader']
+      }
     ]
   },
   plugins: [
